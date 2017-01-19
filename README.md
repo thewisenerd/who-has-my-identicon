1f2a6620e0ad87f1b7423e74fa42bcb24201ca64
thewisenerd <thewisenerd@protonmail.com>
2017-01-19 10:07:59 +0530

    add support to get certain username

--- README.md
+++ README.md
@@ -4,3 +4,17 @@ who-has-my-identicon
 
 _you're ~~identicon is~~ not special. there are a lot of others ~~identicons~~
 just like you._
+
+howto
+-----
+
+1. generate hashes (a 1.2 gigabyte file with hashes of numbers from 1 to 30mil).
+
+```
+./hashgen.py > hashes
+```
+
+2. install requests
+```
+pip install requests
+```
--- generate_regex.py
+++ generate_regex.py
@@ -1,31 +1,90 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-# b9af39401fe82240a310644c54100e87
-# b9af3
-# 9401f
-# e8224
-# 1 0 0 0 1
-# 1 1 0 1 1
-# 1 1 1 1 1
-# 1 0 0 0 1
-# 1 0 0 0 1
+import requests
+import hashlib
+
+import json
+import sys
 
 fg = "[0|2|4|6|8|a|c|e]"
 bg = "[1|3|5|7|9|b|d|f]"
 hexchar = "[0-9a-f]"
 
 pattern = [
- [1, 0, 0, 0, 1],
- [1, 1, 0, 1, 1],
- [1, 1, 1, 1, 1],
- [1, 0, 0, 0, 1],
- [1, 0, 0, 0, 1]
+ [0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0]
 ];
 
 matchHSL = None;
-# matchHSL = "4100e87";
 
 regex = "^";
+
+def getPatternFile(filename):
+	global pattern
+
+	data = None
+	with open(filename) as data_file:
+		try:
+			data = json.load(data_file)
+		except:
+			print("pattern loading failed")
+			exit()
+
+	if data['pattern'] is None:
+		print("invalid pattern file")
+		exit()
+
+	pattern = data['pattern']
+
+def getUserPattern(username):
+	global pattern
+
+	r = requests.get('https://api.github.com/users/' + username)
+
+	if (r.status_code != 200):
+		if (r.status_code == 404):
+			print ("404 not found");
+		else:
+			print ("failed to connect to github API; err code = ", r.status_code)
+		exit()
+
+	try:
+		j = r.json()
+	except:
+		print ("failure decoding json response")
+		exit()
+
+	if j['id'] is None:
+		print ("failure decoding json response")
+		exit()
+
+	userid = str(j['id'])
+	userhash = hashlib.md5(userid.encode('utf-8')).hexdigest()
+
+	for i in range(0, 15):
+		try:
+			c = int( '0x' + userhash[i], 16 )
+		except:
+			print ("failure converting hex to decimal")
+			exit()
+
+		c = (c+1) % 2;
+		if (i < 5):
+			pattern[i][2] = c;
+		elif (i < 10):
+			pattern[i-5][1] = c;
+			pattern[i-5][3] = c;
+		else:
+			pattern[i-10][0] = c;
+			pattern[i-10][4] = c;
+
+if (len(sys.argv) > 1):
+	getUserPattern(sys.argv[1])
+else:
+	getPatternFile("pattern.json")
 
 for i in range(0, 5):
 	if (pattern[i][2]):
